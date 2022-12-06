58be010bba72fd84422497250722a71d7b3e95b8
Fábio Santos <fabio3268@gmail.com>
2022-12-06 06:51:40 -0300

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="header-top-bar">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <ul class="top-bar-info list-inline-item pl-0 mb-0">
+                        <li class="list-inline-item"><a href="mailto:ch-gabdir@ifsul.edu.br"><i class="icofont-support-faq mr-2"></i>ch-gabdir@ifsul.edu.br</a></li>
+                        <li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Rua General Balbão, 81 - Charqueadas - RS </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+                        <a href="tel:+51-3658-3602" >
+                            <span>Contato : </span>
+                            <span class="h4">(51) 3658-3602</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
